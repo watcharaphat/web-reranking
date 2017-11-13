19f81d237b5cd36590d8daac53f103f05de95df0
watcharaphat <watcharaphat.3166@gmail.com>
2017-11-13 22:36:29 +0700

    using glob to list files in directories

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-console.log('hello');
+const glob = require('glob');
+// const recursive = require('recursive-readdir');
 
+// recursive('/Users/watcharaphat/project/web-indexer/data', (error, files) => {
+//   files.forEach((file, index) => {
+//     const fileName = file.slice(45);
+//     const url = 'http://'.concat(fileName);
+//     if (index <= 10) {
+//       console.log(`${index}:\t${url}`);
+//     }
+//   });
+// });
+
+async function getDirectories(src, callback) {
+  const files = await glob.sync(src.concat('/**/*'), callback);
+
+  return files;
+}
+
+async function printDirectories() {
+  const files = await getDirectories('/Users/watcharaphat/project/web-indexer/data');
+  files.forEach((file, index) => {
+    const fileName = file.slice(45);
+    const url = 'http://'.concat(fileName);
+    if (index <= 10) {
+      console.log(`${index}:\t${url}`);
+    }
+  });
+}
+
+printDirectories();
